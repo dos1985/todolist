387a5014aca41b9cbbb0d2529f2e4bfc53f3e512
User <dossovmarlen85@gmail.com>
2023-06-26 09:44:28 +0300

    added env_file

--- todolist/settings.py
+++ todolist/settings.py
@@ -14,13 +14,6 @@ import dotenv
 dotenv.load_dotenv()
 from pathlib import Path
 
-# env = environ.Env(
-#     DEBUG=(bool, False),
-#     SECRET_KEY=(str, ''),
-# )
-# environ.Env.read_env()
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
