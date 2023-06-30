1ff3897eba81cf7cdeced43b94440cab582ca834
User <dossovmarlen85@gmail.com>
2023-06-30 19:20:54 +0300

    allowed hosts vmr-group.kz

--- todolist/settings.py
+++ todolist/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'vmr-group.kz']
 
 
 # Application definition
